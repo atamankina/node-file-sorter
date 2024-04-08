508f0e36fbe5ffd2f61d221b90665a39664a70e2
Galina Atamankina <g.atamankina@gmail.com>
2024-04-08 11:04:33 +0200

    Adding the file sorting logic.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,10 @@
 import * as path from 'node:path'
+import * as fs from 'node:fs'
 
 
 export function extractFileExtensions(dirContent: string[]) {
 
-    const extensions = new Set();
+    const extensions = new Set<string>();
 
     for(let i=0; i < dirContent.length; i++) {
         const fileName = dirContent[i]
@@ -14,3 +15,25 @@ export function extractFileExtensions(dirContent: string[]) {
     return extensions
 }
 
+export function createDirs(folderPath: string, folderNames: string[]): void {
+    // Überprüfen, ob der Hauptordner existiert
+    if (fs.existsSync(folderPath)) {
+        // Wenn vorhanden, nichts tun
+    } else {
+        // Wenn nicht, erstellen
+        fs.mkdirSync(folderPath);
+    }
+
+    // Durchlaufe die Liste der Ordnernamen
+    for (const folderName of folderNames) {
+        const folderFullPath = folderPath + '/' + folderName;
+     // Überprüfen, ob der Ordner bereits existiert
+        if (fs.existsSync(folderFullPath)) {
+            console.log('Ordner "' + folderName + '" existiert bereits.');
+        } else {
+            // Wenn nicht, erstellen
+            fs.mkdirSync(folderFullPath);
+            console.log('Ordner "' + folderName + '" wurde erstellt.');
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { extractFileExtensions } from './helpers'
+import { createDirs } from './helpers'
 
 // Step 1. List the contents of the folder.
 
@@ -12,4 +13,19 @@ console.log(dirContent)
 const extensions = extractFileExtensions(dirContent)
 console.log(extensions)
 
+const extensionsArray = Array.from(extensions)
+console.log(extensionsArray)
 // Step 3. Sort files based on conditions.
+
+createDirs('./output', extensionsArray)
+
+//fs.copyFileSync('./input/', './output/.pdf/document.pdf')
+
+for (const file of dirContent) {
+    const ext = path.extname(file)
+    try {
+        fs.copyFileSync('./input/' + file, './output/' + ext + '/' + file)
+    } catch (e) {
+        console.error(e)
+    }
+}
